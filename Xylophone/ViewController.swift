689547cef6becef85aba9e3f42c34348a16a9419
Mara <kazee.bear@mail.com>
2021-11-10 22:41:54 +0200

    -commit and push to remote

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName:sender.currentTitle!)
+        print(sender.currentTitle!)
+        
+        UIView.animate(withDuration: 0.2){
 
+        sender.alpha = 0.5
+
+        }
+
+        UIView.animate(withDuration: 0.2){
+
+        sender.alpha = 1.0
+
+        }
+    }
     
-
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
